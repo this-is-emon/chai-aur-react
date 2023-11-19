b5a07af98d18b2802e1a7d15a18769494c0d3a27
this-is-emon <mofijul.islam.emon@gmail.com>
2023-11-19 10:13:27 +0600

    Debugging a mistake

--- 06passwordGenerator/src/App.jsx
+++ 06passwordGenerator/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
     for (let i = 1; i <= length; i++) {
       // to get a random number to use as index of 'str'
       let char = Math.floor(Math.random() * str.length + 1);
-      pass = str.charAt(char);
+      pass += str.charAt(char);
     }
 
     setPassword(pass);
