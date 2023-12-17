c9fe08facbe512a8bc451afab9dc7fb6f1f062b3
this-is-emon <mofijul.islam.emon@gmail.com>
2023-12-17 15:29:08 +0600

    Header, Footer primarily added

--- 08reactRouter/src/App.jsx
+++ 08reactRouter/src/App.jsx
@@ -1,9 +1,13 @@
 import "./App.css";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
 
 function App() {
   return (
     <>
+      <Header />
       <div className="bg-green-600 p-4">React Router</div>
+      <Footer />
     </>
   );
 }
