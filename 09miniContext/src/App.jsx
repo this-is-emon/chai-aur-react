2048749468040923a27d311f58d616f44fa288df
this-is-emon <mofijul.islam.emon@gmail.com>
2024-02-01 11:22:55 +0600

    iport 'userContexProvider' inside App.jsx

--- 09miniContext/src/App.jsx
+++ 09miniContext/src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
+import UserContextProvider from "./context/UserContextProvider";
 
 function App() {
   return (
-    <>
+    <UserContextProvider>
       <h1>Vite + React + Context API</h1>
-    </>
+    </UserContextProvider>
   );
 }
 
